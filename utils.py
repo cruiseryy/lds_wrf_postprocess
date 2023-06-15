6ed5a3c8ce0d028054a9f39d7b98556bb383331a
cruiseryy <xp53@cornell.edu>
2023-06-15 11:44:52 +0800

    plotter added

--- utils.py
+++ utils.py
@@ -1,8 +1,21 @@
 import numpy as np
-from matplotlib import pyplot as plt
 import os 
 from collections import Counter
+from time import time
+
+from matplotlib import pyplot as plt
 from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
+from matplotlib.cm import get_cmap
+import matplotlib.ticker as mticker
+
+import cartopy.crs as crs
+from cartopy.feature import NaturalEarthFeature
+import cartopy.io.shapereader as shpreader
+
+import geopandas as gpd
+from netCDF4 import Dataset
+from wrf import (to_np, getvar, smooth2d, get_cartopy, cartopy_xlim,
+                 cartopy_ylim, latlon_coords)
 
 
 class lds_plotter:
@@ -196,9 +209,71 @@ class lds_plotter:
     #     pause = 1
     #     return 
 
+
+class plotter:
+    def __init__(self) -> None:
+        ncfile = Dataset("/home/climate/xp53/testrun_4node/4node_testrun/wrfout_d01_1987-12-01_03:00:00")
+        rain0 = getvar(ncfile, 'RAINNC')
+        self.lats, self.lons = latlon_coords(rain0)
+        self.cart_proj = get_cartopy(rain0)
+        self.coastline = gpd.read_file('/home/climate/xp53/for_plotting/cropped/coastlines.shp')
+        return
+    
+    def plotmap(self, ax, data, title):
+        self.coastline.plot(ax=ax, facecolor='none', edgecolor='black', linewidth=1)
+        ax.contour(to_np(self.lons), to_np(self.lats), to_np(data), 10, colors="none",
+                    transform=crs.PlateCarree())
+        basemap = ax.contourf(to_np(self.lons), to_np(self.lats), to_np(data), 10,
+                    transform=crs.PlateCarree(), cmap="jet")
+        ax.set_extent([np.min(self.lons), np.max(self.lons), np.min(self.lats), np.max(self.lats)], crs=crs.PlateCarree())
+        cbar = plt.colorbar(basemap, ax=ax, orientation='vertical', shrink=.5)
+        cbar.set_label('Rainfall [mm]')
+        cbar.mappable.set_clim(vmin=0, vmax=400) 
+        gl = ax.gridlines(crs=crs.PlateCarree(), draw_labels=True, linewidth=1.5, color='gray', alpha=0.5, linestyle='--')
+        gl.top_labels = False
+        gl.right_labels = False
+        gl.xlocator = mticker.FixedLocator([103.5, 103.7, 103.9, 104.1])
+        gl.ylocator = mticker.FixedLocator([1.1, 1.3, 1.5])
+        ax.set_title(title)
+        return
+
+
+# ncfile = Dataset("/home/climate/xp53/testrun_4node/4node_testrun/wrfout_d01_1987-12-01_03:00:00")
+# rain0 = getvar(ncfile, 'RAINNC')
+
+# lats, lons = latlon_coords(rain0)
+# cart_proj = get_cartopy(rain0)
+
+# coastline = gpd.read_file('/home/climate/xp53/for_plotting/cropped/coastlines.shp')
+
+# fig, ax = plt.subplots(nrows=1,ncols=2,figsize=(12, 6),subplot_kw={'projection': crs.PlateCarree()})
+
+# coastline.plot(ax=ax[0], facecolor='none', edgecolor='black', linewidth=.5)
+# ax[0].contour(to_np(lons), to_np(lats), to_np(rain0), 10, colors="none",
+#             transform=crs.PlateCarree())
+# ax[0].contourf(to_np(lons), to_np(lats), to_np(rain0), 10,
+#              transform=crs.PlateCarree(),
+#              cmap=get_cmap("jet"))
+# ax[0].set_extent([np.min(lons), np.max(lons), np.min(lats), np.max(lats)], crs=crs.PlateCarree())
+
+# ax[1].coastlines('10m', linewidth=0.5)
+# ax[1].contour(to_np(lons), to_np(lats), to_np(rain0), 10, colors="none",
+#             transform=crs.PlateCarree())
+# ax[1].contourf(to_np(lons), to_np(lats), to_np(rain0), 10,
+#              transform=crs.PlateCarree(),
+#              cmap=get_cmap("jet"))
+# ax[1].set_extent([np.min(lons), np.max(lons), np.min(lats), np.max(lats)], crs=crs.PlateCarree())
+
+
+# plt.savefig('test2.pdf')
+
+
 if __name__ == '__main__':
     test = lds_plotter() 
     test.freq_year_plot()
     test.freq_traj_plot()
     test.traj_plot()
+
+    pp = plotter()
+    
     pause = 1
