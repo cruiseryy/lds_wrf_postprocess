9cd531a10a6a828e6ec8aeaac4641c5cf4fc874b
cruiseryy <xp53@cornell.edu>
2023-08-07 17:48:01 +0800

    traj plot, prob est added

--- utilities.py
+++ utilities.py
@@ -6,6 +6,7 @@ import cartopy.crs as ccrs
 from wrf import (to_np, getvar, smooth2d, get_cartopy, cartopy_xlim, cartopy_ylim, latlon_coords)
 
 # /home/climate/xp53/nas_home/LDS_WRF_OUTPUT/K=0.05/0_RAINNC.nc
+# ref = 8.398613461383452 for the whole WRF simulation domain <- from a incorrect K=0 run
 
 class post_analyzer:
     def __init__(self, 
@@ -14,7 +15,8 @@ class post_analyzer:
                  N = 128, 
                  T = 18,
                  dt = 5,
-                 ref = 7.078,
+                 ref = 8.398613461383452,
+                 k = 0,
                  cl_path = '/home/climate/xp53/for_plotting/cropped/coastlines.shp'
                  ) -> None:
         
@@ -24,6 +26,8 @@ class post_analyzer:
         self.T = T
         self.dt = dt
         self.M = self.T * (self.dt + 1)
+
+        self.k = k
 
         # read the coastline
         self.coastline = gpd.read_file(cl_path)
@@ -102,7 +106,7 @@ class post_analyzer:
         train = np.mean(self.rain_order, axis = (2, 3))
         for j in range(self.N):
             res = []
-            for i in range(9):
+            for i in range(self.T - 1):
                 res.append(train[j, (i+1)*6-1] - train[j, (i+1)*6])
             print('The mean rainfall deficit of the {}th ordered traj is {}'.format(j, np.mean(np.abs(res))))
         pause = 1
@@ -111,17 +115,29 @@ class post_analyzer:
     
     def traj_plotter(self, ax):
         # to plot the ordered traj as well as the raw traj (background)
-
         # fill color between the max and min of the raw traj between ts and te
-        for j in range(self.N):
-            for i in range(self.T):
-                ts, te = i * (self.dt + 1), (i + 1) * (self.dt + 1)
-                ax.fill_between(np.arange(ts, te), 
-                                np.min(self.rain_raw[j, ts:te, :, :]), 
-                                np.max(self.rain_raw[j, ts:te, :, :]), 
-                                color='grey', alpha=0.1)
+        idx = [1]
+        for i in range(self.T):
+            ts, te = i * (self.dt + 1) - i, (i + 1) * (self.dt + 1) - i
+            tmp_traj_raw = np.mean(self.rain_raw[:, ts:te, :, :], axis = (2, 3))
+            ax.fill_between(np.arange(ts, te), 
+                                np.min(tmp_traj_raw, axis = 0),
+                                np.max(tmp_traj_raw, axis = 0),
+                                color='grey', alpha=0.25)
+            for j in idx:
+                ind_traj_order = np.mean(self.rain_order[j, ts:te, :, :], axis = (1, 2))
+                ax.plot(np.arange(ts, te), ind_traj_order, color='red', linewidth=1)
+    
+        pause = 1
         return
+    
 
+    def collect_roots(self):
+        self.roots = set([])
+        for j in range(self.N):
+            self.roots.add(self.find_root(j, self.T-1))
+        self.roots = list(self.roots)
+        return
 
     def map_plotter(self, data, ax, var_name = 'Rainfall [mm]'):
         self.coastline.plot(ax=ax, facecolor='none', edgecolor='black', linewidth=1)
@@ -131,9 +147,49 @@ class post_analyzer:
         cbar = plt.colorbar(basemap, ax=ax, orientation='vertical', shrink=.8)
         cbar.set_label(var_name)
         return
+    
+    def weight_est(self):
+        # Iterate over all dt, for each dt, compute the weight of each traj 
+        self.weight_ = np.zeros((self.N, self.T))
+        self.R_ = np.zeros((self.T,))
+        for i in range(self.T-1):
+            for j in range(self.N):
+                ts, te = i * (self.dt + 1), (i + 1) * (self.dt + 1)
+                rain_start = self.rain_raw[j, ts, :, :]
+                rain_end = self.rain_raw[j, te-1, :, :]
+                rain_diff = np.mean(rain_end - rain_start)
+                self.weight_[j, i] = np.exp(self.k * (self.ref * self.dt - rain_diff))
+            self.R_[i] = np.mean(self.weight_[:, i])
+            self.weight_[:, i] /= self.R_[i]
+        # pause = 1 # check if re-estimated weights and Rs are correct
+        return
 
     def agg_weight(self):
         # to compute the aggregated weight of each ordered traj 
+        self.pq_ratio = np.ones((self.N, ))
+        for j in range(self.N):
+            for i in range(self.T - 1):
+                ts, te = i * (self.dt + 1), (i + 1) * (self.dt + 1)
+                rain_start = self.rain_order[j, ts, :,  :]
+                rain_end = self.rain_order[j, te-1, :, :]
+                rain_diff = np.mean(rain_end - rain_start)
+                self.pq_ratio[j] *= (np.exp(-self.k * (self.ref * self.dt - rain_diff)) * self.R_[i])
+                pause = 1
+        
+        # this is for checking consistency of pq_ratio estimations (compute by each sub interval or compute by the whole traj)
+        self.pq_ratio2 = np.zeros((self.N, ))
+        agg_R = 1
+        for i in range(self.T - 1):
+            agg_R *= self.R_[i]
+        for j in range(self.N):
+            ts, te = 0, (self.T - 1) * (self.dt + 1)
+            rain_start = self.rain_order[j, ts, :,  :]
+            rain_end = self.rain_order[j, te-1, :, :]
+            rain_diff = np.mean(rain_end - rain_start)
+            self.pq_ratio2[j] = np.exp(-self.k * (self.ref * self.dt * (self.T - 1) - rain_diff)) * agg_R
+            pause = 1
+        
+        pause = 1 # check if pq_ratio is correct
         return
     
     def return_period(self):
@@ -142,13 +198,19 @@ class post_analyzer:
 
         
 if __name__ == '__main__':
-    tmp = post_analyzer(path = "/home/climate/xp53/nas_home/LDS_WRF_OUTPUT/K=0", T = 10)
+    tmp = post_analyzer(path = "/home/climate/xp53/nas_home/LDS_WRF_OUTPUT/K=0", T = 18)
     tmp.var_read()
     tmp.order_()
+    tmp.weight_est()
+    tmp.agg_weight()
+    tmp.collect_roots()
+    fig, ax = plt.subplots(figsize=(8, 6))
+    tmp.traj_plotter(ax)
+    fig.savefig('test.pdf')
+    pause = 1
     fig, ax = plt.subplots(figsize=(8, 6), subplot_kw={'projection': ccrs.PlateCarree()})
     jj = 99
     ii = 1
     tmp.map_plotter(tmp.rain_raw[jj, ii, :, :], ax)
     fig.savefig('test.pdf')
     pause = 1
-    
