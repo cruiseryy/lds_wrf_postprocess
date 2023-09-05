7b72e669e3da0a32147d2198c1d29d23b61a7e72
cruiseryy <xp53@cornell.edu>
2023-09-05 22:10:32 +0800

    masking and new plotting features

--- utilities.py
+++ utilities.py
@@ -6,6 +6,7 @@ import cartopy.crs as ccrs
 from wrf import (to_np, getvar, smooth2d, get_cartopy, cartopy_xlim, cartopy_ylim, latlon_coords)
 import matplotlib.ticker as mticker
 import matplotlib as mpl
+import numpy.matlib
 
 # /home/climate/xp53/nas_home/LDS_WRF_OUTPUT/K=0.05/0_RAINNC.nc
 # ref = 8.398613461383452 for the whole WRF simulation domain <- from a incorrect K=0 run
@@ -54,6 +55,8 @@ class post_analyzer:
         # read the initial condition (IC) years
         self.ic = np.loadtxt(self.path + '/VARS/' + 'ic.txt').astype(int)
         self.bc_record = np.loadtxt(self.path + '/VARS/' + 'bc_record.txt').astype(int)
+
+        self.mask = np.loadtxt('mask.txt')
 
         pause = 1
         return
@@ -153,15 +156,15 @@ class post_analyzer:
         self.coastline.plot(ax=ax, facecolor='none', edgecolor='black', linewidth=1)
         if align:
             # a trick to set the colorbar range
-            v = np.linspace(crange[0], crange[1], 10, endpoint=True)
+            v = np.linspace(crange[0], crange[1], 20, endpoint=True)
             basemap = ax.contourf(self.lonc, self.latc, data, v, 
                                     transform=ccrs.PlateCarree(), cmap=cmap)
         else:
             basemap = ax.contourf(self.lonc, self.latc, data, 10, 
                                     transform=ccrs.PlateCarree(), cmap=cmap)
         basemap.set_clim(crange[0], crange[1])
-        ax.set_extent([np.min(self.lonc), np.max(self.lonc), np.min(self.latc), np.max(self.latc)], crs=ccrs.PlateCarree())
-        # ax.set_extent([103.58, 104.12, 1.153, 1.502], crs=ccrs.PlateCarree())
+        # ax.set_extent([np.min(self.lonc), np.max(self.lonc), np.min(self.latc), np.max(self.latc)], crs=ccrs.PlateCarree())
+        ax.set_extent([103.599, 104.101, 1.15, 1.501], crs=ccrs.PlateCarree())
         gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True, linewidth=1.5, color='gray', alpha=0.5, linestyle='--')
         gl.top_labels = False
         gl.right_labels = False
@@ -182,8 +185,8 @@ class post_analyzer:
         con_prob = 0
         for i in ls:
             idx = self.rank[i][0]
-            con_rain += self.rain_orderc[idx, :, :, :] * self.pq_ratio[idx] * 1 / (self.N + 1)
-            con_prob += self.pq_ratio[idx] * 1 / (self.N + 1)
+            con_rain += self.rain_orderc[idx, :, :, :] * self.pq_ratio[idx] * 1 / self.N
+            con_prob += self.pq_ratio[idx] * 1 / self.N
         print([con_prob, 1 / con_prob])
         con_rain /= con_prob
         return con_rain
@@ -243,12 +246,6 @@ class post_analyzer:
         # read hisotrical era run rainfall data for comparison
         rainh2 = np.loadtxt('/home/climate/xp53/wrf_lds_post/rain.txt')
         rainh = np.loadtxt('/home/climate/xp53/wrf_lds_post/rainc.txt')
-        
-        # find the idx of the second smallest element in rainh 
-        # tmp = list(zip(range(rainh.shape[0]), rainh))
-        # tmp.sort(key = lambda x: x[1])
-        # df = np.diff([i[1] for i in tmp])
-
         pause = 1
 
         rainh.sort()
@@ -278,16 +275,28 @@ class post_analyzer:
         XX = np.vstack([x, np.ones(len(x))]).T
         slope, intercept = np.linalg.lstsq(XX, y, rcond=None)[0]
 
-        # fig, ax = plt.subplots(figsize=(8, 6))
-        # ax.scatter(accu_rain2[:, -1], accu_rain[:, -1], color = 'red', linewidth=1)
-        # ax.plot(accu_rain2[:, -1], slope * accu_rain2[:, -1] + intercept, color = 'blue', linewidth=1)
-        # ax.set_xlabel('Original Accumulative Rainfall [mm]')
-        # ax.set_ylabel('Cropped Accumulative Rainfall [mm]')
-        # # grid
-        # ax.grid(which='major', axis='x', linestyle='-', linewidth=1, color='grey', alpha=0.5)
-        # ax.grid(which='major', axis='y', linestyle='-', linewidth=1, color='grey', alpha=0.5)
-        # fig.savefig('rain_comp.pdf')
-        # pause = 1
+        fig, ax = plt.subplots(nrows = 1, ncols = 2, figsize=(14, 6))
+        ax[0].scatter(accu_rain2[:, -1], accu_rain[:, -1], color = 'red', linewidth=1)
+        ax[0].plot(accu_rain2[:, -1], slope * accu_rain2[:, -1] + intercept, color = 'blue', linewidth=1)
+        ax[0].set_xlabel('Original Accumulative Rainfall [mm]')
+        ax[0].set_ylabel('Cropped Accumulative Rainfall [mm]')
+        # grid
+        ax[0].grid(which='major', axis='x', linestyle='-', linewidth=1, color='grey', alpha=0.5)
+        ax[0].grid(which='major', axis='y', linestyle='-', linewidth=1, color='grey', alpha=0.5)
+
+        sg_mask_rain = np.multiply(self.rain_order[:, :, :, :], self.mask)
+        accu_rain3 = np.nanmean(sg_mask_rain[:, :, :, :], axis = (2, 3))
+        y2 = accu_rain3[:, -1]
+        slope2, intercept2 = np.linalg.lstsq(XX, y2, rcond=None)[0]
+        ax[1].scatter(accu_rain2[:, -1], accu_rain3[:, -1], color = 'red', linewidth=1)
+        ax[1].plot(accu_rain2[:, -1], slope2 * accu_rain2[:, -1] + intercept2, color = 'blue', linewidth=1)
+        ax[1].set_xlabel('Original Accumulative Rainfall [mm]')
+        ax[1].set_ylabel('SG masked Accumulative Rainfall [mm]')
+        # grid
+        ax[1].grid(which='major', axis='x', linestyle='-', linewidth=1, color='grey', alpha=0.5)
+        ax[1].grid(which='major', axis='y', linestyle='-', linewidth=1, color='grey', alpha=0.5)
+        fig.savefig('rain_comp.pdf')
+        pause = 1
         
         cdf2 = np.zeros((self.N, ))
         cdf2[0] = 1 / self.N * self.pq_ratio[rank2[0][0]]
@@ -296,7 +305,7 @@ class post_analyzer:
         return_period2 = 1 / cdf2
 
         cdf = np.zeros((self.N, ))
-        cdf[0] = 1 / (self.N+1) * self.pq_ratio[rank[0][0]]
+        cdf[0] = 1 / self.N * self.pq_ratio[rank[0][0]]
         for j in range(1, self.N):
             cdf[j] = cdf[j-1] + 1 / self.N * self.pq_ratio[rank[j][0]]
         return_period = 1 / cdf
@@ -355,6 +364,15 @@ if __name__ == '__main__':
     fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(16, 6), subplot_kw={'projection': ccrs.PlateCarree()})
     tmp.map_plotter(tmp.rain_orderc[tmp.rank[0][0],-1,:,:], ax[0])
     tmp.map_plotter(tmp.rain_orderc[tmp.rank[1][0],-1,:,:], ax[1])
+    density = 7
+    ax[0][0].contourf(
+        tmp.lons, tmp.lats, tmp.mask == 1,
+        transform=ccrs.PlateCarree(),
+        colors='none',
+        levels=[.5,1.5],
+        hatches=[density*'/',density*'/'],
+    )
+    
 
     fig.savefig('dry_map.pdf')
     pause = 1
